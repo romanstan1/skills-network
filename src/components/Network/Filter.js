e71730edb40482b3d3880b84d3bf23b52d05d9bf
Roman Stankiewicz <roman.stan3@gmail.com>
2018-03-08 10:50:43 +0000

    filter and edit profile tabs

--- src/components/Network/Filter.js
+++ src/components/Network/Filter.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 export default class Filter extends Component {
   state = {
-    open: false
+    open: false,
+    nav: 'Filter'
   }
 
   toggleOpen = () => {
     this.setState({open: !this.state.open})
+  }
+  selectNav = (e) => {
+    e.stopPropagation()
+    this.setState({nav: e.target.innerHTML})
   }
 
   render() {
@@ -15,11 +20,18 @@ export default class Filter extends Component {
         className={this.state.open? 'filter-panel open': 'filter-panel closed'}
         onClick={this.toggleOpen}>
         <div className='chevron'/>
-        <p>
-          Filter panel
-          <br/><br/>
-          Profile
-        </p>
+
+        <div className='filter-inner'>
+          <nav>
+            <span className={this.state.nav === "Filter"? "selected": null}
+              onClick={this.selectNav}>Filter</span>
+            <span className={this.state.nav === "Edit Profile"? "selected": null}
+              onClick={this.selectNav}>Edit Profile</span>
+          </nav>
+          <section>
+
+          </section>
+        </div>
       </div>
     ]
   }
