ad6ad69b6e9854981eb4384fd68cfcd5a5420022
Roman Stankiewicz <roman.stan3@gmail.com>
2018-04-27 07:20:16 +0100

    toggling between 2d and 3d

--- src/Home/Network/index.js
+++ src/Home/Network/index.js
@@ -11,23 +11,24 @@ import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from 'react-force-graph';
 import {peopleColour, skillsColour, connectionsColour} from '../../styles/theme'
 import SelectDimension from './SelectDimension'
 
-
-
-
-
-
-
 class Network extends Component {
 
   state = {
     nodes: [],
-    links: []
+    links: [],
+    width: window.innerWidth,
+    height: window.innerHeight
   }
 
   componentDidMount() {
+    window.addEventListener("resize", this.updateScreenDimensions);
     const {nodes, links} = this.constructForceNetwork(this.props)
     this.setState({nodes, links})
     // render(nodes, links)
+  }
+
+  updateScreenDimensions = () => {
+    this.setState({width: window.innerWidth, height: window.innerHeight})
   }
 
   componentWillReceiveProps(nextProps) {
@@ -56,7 +57,6 @@ class Network extends Component {
   }
 
   setNodeSize = (d) => {
-    // console.log("d",d)
     if(d.type === 'skill') {
       return Math.pow((d.hadBy.length), 3) * 0.0005
     } else {
@@ -71,9 +71,6 @@ class Network extends Component {
   }
 
   setLinkColor = (d) => {
-    // if(d.type === 'currentSkills') return 0.1
-    // else if(d.type === 'desiredSkills') return 0.1
-    // return 100
     return '#b78d5d'
   }
 
@@ -84,7 +81,7 @@ class Network extends Component {
   }
 
   render() {
-    const {nodes, links} = this.state
+    const {nodes, links, width, height} = this.state
     return [
       // <svg key='svg'></svg>,
       // <canvas key='canvas' ref="canvas" style={{width:'100%', height:'100%'}}/>,
@@ -95,26 +92,35 @@ class Network extends Component {
       //   // style={{width:'100%', height:'100%'}}
       // />,
       <div id='threedGraph' key='3d'>
-        <ForceGraph3D
-          showNavInfo={false}
-          width={window.innerWidth - 260}
-          graphData={{nodes, links}}
-          nodeAutoColorBy='type'
-          nodeOpacity={0.97}
-          nodeResolution={50}
-          d3Force={'charge', null}
-          d3Force={'link', null}
-          backgroundColor='rgba(255, 0, 0, 0)'
-          d3VelocityDecay={0.9}
-          d3AlphaDecay={0.08}
-          nodeVal={this.setNodeSize}
-          nodeColor={this.setNodeColor}
-          linkColor={this.setLinkColor}
-          linkWidth={this.setLinkWidth}
-          linkOpacity={0.4}
-          linkResolution={20}
-
-        />
+        {
+          this.props.dimension === '3D' ?
+          <ForceGraph3D
+            showNavInfo={false}
+            width={width - 260}
+            height={height}
+            graphData={{nodes, links}}
+            nodeAutoColorBy='type'
+            nodeOpacity={0.97}
+            nodeResolution={50}
+            d3Force={'charge', null}
+            d3Force={'link', null}
+            backgroundColor='rgba(255, 0, 0, 0)'
+            d3VelocityDecay={0.9}
+            d3AlphaDecay={0.08}
+            nodeVal={this.setNodeSize}
+            nodeColor={this.setNodeColor}
+            linkColor={this.setLinkColor}
+            linkWidth={this.setLinkWidth}
+            linkOpacity={0.4}
+            linkResolution={20}
+          />
+          :
+          <div
+            id='3d-graph'
+            ref="3d-graph" key='3d-graph'
+            style={{width, height}}
+          />
+        }
       </div>,
       <FullScreenIcon key='fullscreenicon'/>,
       <SelectDimension key='selectdimension'/>
@@ -125,5 +131,6 @@ class Network extends Component {
 export default connect(state => ({
   people: state.data.people.filters.filter(person => person.active && !person.connectionFilterActive),
   skills: state.data.skills.filters.filter(skill => skill.active),
-  connections: state.data.connections
+  connections: state.data.connections,
+  dimension: state.data.dimension
 }))(Network)
