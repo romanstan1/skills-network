e16a6bbc9f6782dad9c4194e1fa4f5a5c11caf3f
Roman Stankiewicz <roman.stan3@gmail.com>
2018-07-13 10:01:56 +0100

    just before canvas rendering

--- src/Home/Network/TwoDNetwork/index.js
+++ src/Home/Network/TwoDNetwork/index.js
@@ -1,29 +1,32 @@
 import React, {Component} from 'react';
-import {render, update} from './D3_modules'
+// import {render2, update2} from './D3_modules'
+import {render2, update2} from './D3_modules_rewrite'
 import {connect} from 'react-redux'
 
 class TwoDNetwork extends Component {
 
   componentDidMount() {
     const {nodes, links, width, height} = this.props
-    render(width, height)
+    // render3(width, height)
+    render2(nodes, links, width, height)
   }
 
   componentWillReceiveProps(nextProps) {
     const {nodes, links, width, height} = nextProps
-    update(nodes, links)
+    // update(nodes, links)
+    update2(nodes, links, width, height)
   }
 
   render() {
     const {width, height} = this.props
     return (
-      <div id='twod-graph' ref="twod-graph" key='twod-graph'
-        style={{width, height}}
-      />
+      // <div id='twod-graph' ref="twod-graph" key='twod-graph'
+      //   style={{width, height}}
+      // />
+      <svg style={{width: width - 260, height}} key='svg'></svg>
     )
   }
 }
-// {/* <svg style={{width: width - 260, height}} key='svg'></svg> */}
 
 export default connect(state => ({
   links: state.data.links,
