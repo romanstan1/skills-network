a6af1e7fa0e2ad078c60c523c0904bc6dfc6c1f1
Roman Stankiewicz <roman.stan3@gmail.com>
2018-07-17 00:00:58 +0100

    delay data fetch

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -1,10 +1,17 @@
 import {seededSkillsData} from './seed.js'
 
-export function getData() {
-  return fetch(`https://serene-ocean-70888.herokuapp.com/skillsMatrix`)
-  .then(res => res.json())
-  .catch(error => {
+const URL = `https://serene-ocean-70888.herokuapp.com/skillsMatrix`
+
+export async function getData() {
+  const delay = time => new Promise(res=>setTimeout(()=>res(),time));
+  await delay(2000);
+
+  try {
+    const data = await fetch(URL)
+    return await data.json()
+  }
+  catch(error) {
     console.log(error)
     return 'Error'
-  })
+  }
 }
